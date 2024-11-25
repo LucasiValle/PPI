62230b23feb638e64ce219ac920ed08550938d78
LucasiValle <lucas.irvalle@gmail.com>
2024-11-25 11:52:59 -0300

    ListaPHP

---
+++ web/ListaPHP/Questao1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+Temos as tags php e echo
+
+<?php
+
+$nome = "Lucas";
+$idade = 18;
+
+echo "Olá, meu nome é $nome e tenho $idade anos.";
+?>
+</body>
+</html>
---
+++ web/ListaPHP/Questao2.php
@@ -0,0 +1,3 @@
+O php é uma linguagem de programação interpretada executada no servidor, diferente do javascript
+que é executado do lado cliente. o codigo php é executado a partir de uma requisição HTTP que 
+chega no servidor HTTP
